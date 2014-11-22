2307f38d9c70f58956a726f51f0349d93505d7cb
knemoto2 <nemo10@gmail.com>
2014-11-22 14:01:34 +0900

    refactoring

--- src/test/groovy/tddbc/TodoTest.groovy
+++ src/test/groovy/tddbc/TodoTest.groovy
@@ -6,31 +6,26 @@ import spock.lang.Specification
  * Created by knemoto2 on 2014/11/22.
  */
 class TodoTest extends Specification {
+    def todo = new Todo()
 
     def "TODOを追加できる"() {
-        when:
-        Todo todo=new Todo()
-        then:
-        todo.add("なにすかること") == 1
+        expect:
+        todo.add("task") == 1
+
     }
 
 
     def "最後に追加したTODOのみの詳細を見れる"() {
         when:
-        Todo todo=new Todo()
-        todo.add("new task")
+        todo.add(input)
         then:
-        todo.last() == "new task"
+        todo.last() == output
+        where:
+        input           | output
+        "new task"      | "new task"
+        "new task 2"    | "new task 2"
     }
 
-
-    def "最後に追加したTODOのみの詳細を見れる2"() {
-        when:
-        Todo todo=new Todo()
-        todo.add("new task 2")
-        then:
-        todo.last() == "new task 2"
-    }
 
 
 
