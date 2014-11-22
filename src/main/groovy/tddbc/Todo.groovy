33570a8fc503c103285065f3f7498005f780b40a
knemoto2 <nemo10@gmail.com>
2014-11-22 13:45:49 +0900

    add function

--- src/main/groovy/tddbc/Todo.groovy
+++ src/main/groovy/tddbc/Todo.groovy
@@ -4,8 +4,10 @@ package tddbc
  * Created by knemoto2 on 2014/11/22.
  */
 class Todo {
+    private def todo = []
 
     int add(String s) {
+        todo.add(s)
         return 1
     }
 }
--- src/test/groovy/tddbc/TodoTest.groovy
+++ src/test/groovy/tddbc/TodoTest.groovy
@@ -16,6 +16,7 @@ class TodoTest extends Specification {
 
 
 
+
 }
 
 
